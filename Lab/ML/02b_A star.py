ae3f855f583778ae1ebedee2720f9e13f3cfd1cc
Nikhil Nayak <117031804+20Nikhil02@users.noreply.github.com>
2023-07-09 19:15:07 +0530

    Update 02b_A star.py

--- Lab/ML/02b_A star.py
+++ Lab/ML/02b_A star.py
@@ -1,104 +1,76 @@
-#Write a program to implement the A* algorithm
 import heapq
 
-def a_star(graph, start, goal, heuristic):
-    # Initialize the priority queue with the start node
-    frontier = [(0 + heuristic[start], start)]
-    # Initialize the cost dictionary with the start node
-    cost = {start: 0}
-    # Initialize the parent dictionary with the start node
-    parent = {start: None}
-    # Initialize the explored set
-    explored = set()
+class Node:
+    def __init__(self, position, parent=None):
+        self.position = position
+        self.parent = parent
+        self.g = 0  # Cost from start node to current node
+        self.h = 0  # Heuristic (estimated) cost from current node to goal node
+        self.f = 0  # Total cost (g + h)
 
-    # Loop until the frontier is empty
-    while frontier:
-        # Pop the node with the lowest f-score
-        (f_score, current_node) = heapq.heappop(frontier)
+    def __lt__(self, other):
+        return self.f < other.f
 
-        # Check if the current node is the goal
-        if current_node == goal:
-            # Reconstruct the path from the goal to the start
+def astar_search(graph, start, goal, heuristic):
+    open_list = []
+    closed_set = set()
+
+    start_node = Node(start)
+    goal_node = Node(goal)
+    heapq.heappush(open_list, start_node)
+
+    while open_list:
+        current_node = heapq.heappop(open_list)
+        closed_set.add(current_node.position)
+
+        if current_node.position == goal_node.position:
             path = []
-            while current_node is not None:
-                path.append(current_node)
-                current_node = parent[current_node]
-            path.reverse()
-            return (path, cost[goal])
+            while current_node:
+                path.append(current_node.position)
+                current_node = current_node.parent
 
-        # Add the current node to the explored set
-        explored.add(current_node)
+            return path[::-1]
 
-        # Explore the neighbors of the current node
-        for neighbor, neighbor_cost in graph[current_node]:
-            # Calculate the tentative g-score
-            tentative_g_score = cost[current_node] + neighbor_cost
-            # Check if the neighbor is already in the explored set
-            if neighbor in explored:
-                # If the tentative g-score is higher than the current g-score, skip this neighbor
-                if tentative_g_score >= cost.get(neighbor, float('inf')):
-                    continue
+        neighbors = graph[current_node.position]
 
-            # Check if the neighbor is not in the frontier or the tentative g-score is lower than the current g-score
-            if neighbor not in [node[1] for node in frontier] or tentative_g_score < cost.get(neighbor, float('inf')):
-                # Update the cost and parent dictionaries
-                cost[neighbor] = tentative_g_score
-                parent[neighbor] = current_node
-                # Add the neighbor to the frontier with its priority being its f-score
-                heapq.heappush(frontier, (tentative_g_score + heuristic[neighbor], neighbor))
+        for neighbor in neighbors:
+            neighbor_node = Node(neighbor, current_node)
+            if neighbor in closed_set:
+                continue
 
-    # If the goal cannot be reached, return None
-    return None
+            neighbor_node.g = current_node.g + 1
+            neighbor_node.h = heuristic[neighbor]
+            neighbor_node.f = neighbor_node.g + neighbor_node.h
 
-# Example graph
+            heapq.heappush(open_list, neighbor_node)
+
+    return []
+
+# Example usage:
 graph = {
-    'A': [('B', 5), ('C', 6)],
-    'B': [('D', 4), ('E', 7)],
-    'C': [('F', 9), ('G', 8)],
-    'D': [('H', 3)],
-    'E': [('I', 6)],
-    'F': [('J', 5)],
-    'G': [('K', 7)],
-    'H': [('L', 1)],
-    'I': [('M', 2)],
-    'J': [('N', 3)],
-    'K': [('O', 4)],
-    'L': [],
-    'M': [],
-    'N': [],
-    'O': [('P', 1)],
-    'P': []
+    'A': ['B', 'C'],
+    'B': ['A', 'C', 'D'],
+    'C': ['A', 'B', 'D'],
+    'D': ['B', 'C', 'E'],
+    'E': ['D']
 }
 
-# Heuristic function
+start = 'A'
+goal = 'E'
+
 heuristic = {
-    'A': 10,
-    'B': 8,
-    'C': 7,
-    'D': 6,
-    'E': 8,
-    'F': 3,
-    'G': 2,
-    'H': 5,
-    'I': 6,
-    'J': 3,
-    'K': 2,
-    'L': 1,
-    'M': 4,
-    'N': 2,
-    'O': 4,
-    'P': 0
+    'A': 4,
+    'B': 2,
+    'C': 3,
+    'D': 1,
+    'E': 0
 }
 
-# Get start and goal nodes from the user
-start = input("Enter the start node: ")
-goal = input("Enter the goal node: ")
+path = astar_search(graph, start, goal, heuristic)
 
-# Run the Best First Search algorithm
-result = a_star(graph, start, goal,heuristic)
-
-# Print the result
-if result is not None:
-    print(f"The minimum cost from {start} to {goal} is {result}.")
+if path:
+    print("Path found:")
+    for node in path:
+        print(node)
 else:
-    print(f"There is no path from {start} to {goal}.")
+    print("No path found.")
